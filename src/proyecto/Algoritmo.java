b28ba6d972d7c502186367b4d33e35ef25a48cce
Emmanuel <78392931+EmmanuelPred@users.noreply.github.com>
2021-04-23 15:22:50 -0500

    Update Algoritmo.java

--- src/proyecto/Algoritmo.java
+++ src/proyecto/Algoritmo.java
@@ -23,33 +23,33 @@ public class Algoritmo {
     public double[] puntos(double min,double max){
         double celda=(max-min)/3;
         almacenar[0]=min;
-        almacenar[1]=min+celda;                         // Este método ayuda a determinar los puntos candidatos (intersecciones) a centroide. 
+        almacenar[1]=min+celda;                        
         almacenar[2]=almacenar[1]+celda;
         almacenar[3]=max;
         return almacenar;
        }
   
     public double Distancia(double x1, double x2, double y1, double y2){
-    dist=Math.hypot((x2-x1), (y2-y1));                                      //Con este método se determinan las distancias entre cada intersección y cada dato que se ingresó.
+    dist=Math.hypot((x2-x1), (y2-y1));                                     
         return dist;
     }  
    
     public double fmont(double sigma, double dist){
-        result= Math.exp(-((Math.pow(dist,2))/(2*(Math.pow(sigma,2)))));        //Este es el método con el cual se calcula la función montaña.
+        result= Math.exp(-((Math.pow(dist,2))/(2*(Math.pow(sigma,2)))));        
         return result;
     }
    
      public double fdesc(double beta, double dist){
-        resultado= Math.exp(-((Math.pow(dist,2))/(2*(Math.pow(beta,2)))));      //Este es el método con el cual se calcula una de las partes de la función de deconstrucción.
+        resultado= Math.exp(-((Math.pow(dist,2))/(2*(Math.pow(beta,2)))));     
         return resultado;
        } 
     
-     public double fdesc2(double mc1, double mI, double fdesc){                    //Con este método se lleva acabo el cálculo total de la función de deconstrucción
+     public double fdesc2(double mc1, double mI, double fdesc){                    
              resultado2=mI-(mc1*fdesc); 
          return resultado2;
      }  
       
-    public int centroid(double []vec){                                             //Este método nos sirvió para determinar cual de los puntos candidatos o intersecciones del enrrejado era el mas optimo
+    public int centroid(double []vec){                                            
             mayor=vec[0];
             for(int i=0;i<vec.length;i++){
                 if(vec[i]>mayor){
